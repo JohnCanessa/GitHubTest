04d833ec70cb4848c6833b538c6452b7c4082f6c
JohnCanessa <john.canessa@gmail.com>
2020-02-15 11:10:22 -0600

    added good bye message

--- Solution.java
+++ Solution.java
@@ -1,5 +1,7 @@
 /**
  * Author: John Canessa - john.canessa@gmail.com
+ * 
+ * Testing a set of git commands to compare with the gh CLI.
  */
 public class Solution {
 
@@ -10,5 +12,8 @@ public class Solution {
 
     // **** display welcome message ****
     System.out.println("main <<< welcome !!!");
+
+    // **** display good bye message ****
+    System.out.println("main <<< bye bye");
   }
 }
