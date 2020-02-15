84c743be5ae39d7aa1869b78e488b8780fafb3d4
JohnCanessa <john.canessa@gmail.com>
2020-02-15 10:54:53 -0600

    added Solutions.java

---
+++ Solution.java
@@ -0,0 +1,14 @@
+/**
+ * Author: John Canessa - john.canessa@gmail.com
+ */
+public class Solution {
+
+  /**
+   * Test scaffolding.
+   */
+  public static void main(String[] args) {
+
+    // **** display welcome message ****
+    System.out.println("main <<< welcome !!!");
+  }
+}
