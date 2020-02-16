a95fc25938b1700430cbcb470916c736b18f0405
JohnCanessa <john.canessa@gmail.com>
2020-02-16 08:48:12 -0600

    added displayMessage function

--- Solution.java
+++ Solution.java
@@ -6,12 +6,36 @@
 public class Solution {
 
   /**
-   * Test scaffolding.
+   * This function loops displaying a message the number of times specified.
    */
-  public static void main(String[] args) {
+  /**
+   * Test scaffolding.
+   * 
+   * @throws InterruptedException
+   */
+  static void displayMessage(long delay, int times, String msg) throws InterruptedException {
+
+    // **** loop displaying message ****
+    for (int i = 0; i < times; i++) {
+
+      // **** display message ****
+      System.out.println("displayMessage <<< i: " + i + " msg ==>" + msg + "<==");
+
+      // **** delay ****
+      Thread.sleep(delay);
+    }
+  }
+
+  public static void main(String[] args) throws InterruptedException {
 
     // **** display welcome message ****
     System.out.println("main <<< welcome !!!");
+
+    // **** display messages a number of times ****
+    long delay = 1000;
+    int times = 7;
+    String msg = "working hard ...";
+    displayMessage(delay, times, msg);
 
     // **** display good bye message ****
     System.out.println("main <<< bye bye");
